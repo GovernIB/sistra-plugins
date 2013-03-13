989299eb722b0312aa6e795289ff328afa6eecf6
amaciam-indra <amaciam-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2013-03-13 07:42:08 +0000

    mid: 0000245 msg: Modificación del formateador específico de Hojas de Reclamaciones para añadir pie de páginas
    git-svn-id: https://svn.code.sf.net/p/sistra/code/plugins-caib/trunk@669 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- moduls/formateadores/moduls/formateadores/src/es/caib/redose/persistence/formateadores/FormateadorPdfReclamaciones.java
+++ moduls/formateadores/moduls/formateadores/src/es/caib/redose/persistence/formateadores/FormateadorPdfReclamaciones.java
@@ -57,36 +57,36 @@ public class FormateadorPdfReclamaciones extends FormateadorPdfFormularios{
 			
 			// Para cada copia generamos 3 copias: administracion, empresa e interesado y
 			// stampamos el num secuencial
-			ObjectStamp textos [] = new ObjectStamp[1];
-			//textos[0] = new TextoStamp();
-			//textos[0].setPage(0);
-			//textos[0].setX(150);
-			//textos[0].setY(20);		
-			//textos[0].setRotation(0);		
+			ObjectStamp textos [] = new ObjectStamp[2];
 			textos[0] = new TextoStamp();
-			textos[0].setPage(1);
-			textos[0].setX(460);
-			textos[0].setY(756);		
-			textos[0].setRotation(0);
-			((TextoStamp) textos[0]).setTexto(anyo + "/" + Long.toString(numSecuenciaInicio + i));
+			textos[0].setPage(0);
+			textos[0].setX(40);
+			textos[0].setY(20);		
+			textos[0].setRotation(0);		
+			textos[1] = new TextoStamp();
+			textos[1].setPage(1);
+			textos[1].setX(460);
+			textos[1].setY(756);		
+			textos[1].setRotation(0);
+			((TextoStamp) textos[1]).setTexto(anyo + "/" + Long.toString(numSecuenciaInicio + i));
 			
 			
 			// Ejemplar administracion
-			//((TextoStamp) textos[0]).setTexto("Ejemplar para la administración / Exemplar per l'administració");
+			((TextoStamp) textos[0]).setTexto("Exemplar per a l'administració / Ejemplar para la administración");
 			bis = new ByteArrayInputStream(documentoF.getDatosFichero());
 			bos = new ByteArrayOutputStream(sizeBuffer);
 			UtilPDF.stamp(bos,bis,textos);
 			administracion = bos.toByteArray();
 			
 			// Ejemplar empresa
-			//((TextoStamp) textos[0]).setTexto("Ejemplar para la empresa / Exemplar per l'empresa");
+			((TextoStamp) textos[0]).setTexto("Exemplar per a l'empresa reclamada o denunciada / Ejemplar para la empresa reclamada o denunciada");
 			bis = new ByteArrayInputStream(documentoF.getDatosFichero());
 			bos = new ByteArrayOutputStream(sizeBuffer);
 			UtilPDF.stamp(bos,bis,textos);
 			empresa = bos.toByteArray();
 			
 			// Ejemplar interesado
-			//((TextoStamp) textos[0]).setTexto("Ejemplar para el interesado / Exemplar per l'interesat");
+			((TextoStamp) textos[0]).setTexto("Exemplar per a la persona reclamant o denunciant / Ejemplar para la persona reclamante o denunciante");
 			bis = new ByteArrayInputStream(documentoF.getDatosFichero());
 			bos = new ByteArrayOutputStream(sizeBuffer);
 			UtilPDF.stamp(bos,bis,textos);
