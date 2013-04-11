140dea8445781710a31625059e203f594fcc46e8
amaciam-indra <amaciam-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2013-04-11 11:00:46 +0000

    mid: 0000257 msg: Modificación del formateador específico del IBAVI para generar una única copia por hoja
    git-svn-id: https://svn.code.sf.net/p/sistra/code/plugins-caib/trunk@715 5878cf19-a0df-4652-bd9f-a806d42fd1e1

--- moduls/formateadores/moduls/formateadores/src/es/caib/redose/persistence/formateadores/FormateadorPdfIbavi.java
+++ moduls/formateadores/moduls/formateadores/src/es/caib/redose/persistence/formateadores/FormateadorPdfIbavi.java
@@ -85,16 +85,17 @@ public class FormateadorPdfIbavi extends FormateadorPdfFormularios{
 		// - Generamos n pdfs (cada copia -> 3 pdfs)
 		int sizeBuffer = plantilla.getArchivo().getDatos().length + 1024;
 		ByteArrayOutputStream bos;
-		byte[] administracion,interesado,empresa;
+		byte[] administracion = null;
 		int numIS = 0;
-		ByteArrayInputStream [] iss = new ByteArrayInputStream[3 * numCopias];
+		//ByteArrayInputStream [] iss = new ByteArrayInputStream[3 * numCopias];
+		ByteArrayInputStream [] iss = new ByteArrayInputStream[numCopias];
 		
 		
 		while(numeros.hasMoreElements() && referencias.hasMoreElements() && identificadores.hasMoreElements()){
 			
 			// Para cada copia generamos 3 copias: administracion, empresa e interesado y
 			// stampamos el num secuencial
-			ObjectStamp textos [] = new ObjectStamp[7];
+			ObjectStamp textos [] = new ObjectStamp[3];
 			textos[0] = new TextoStamp();
 			textos[0].setPage(0);
 			textos[0].setRotation(0);		
@@ -102,7 +103,7 @@ public class FormateadorPdfIbavi extends FormateadorPdfFormularios{
 			textos[1].setPage(1);
 			textos[2] = new TextoStamp();
 			textos[2].setPage(1);
-			textos[3] = new TextoStamp();
+			/*textos[3] = new TextoStamp();
 			textos[3].setPage(1);
 			textos[4] = new TextoStamp();
 			textos[4].setPage(1);
@@ -112,67 +113,67 @@ public class FormateadorPdfIbavi extends FormateadorPdfFormularios{
 			textos[5].setRotation(0);
 			textos[6] = new TextoStamp();
 			textos[6].setPage(1);
-			textos[6].setRotation(0);
+			textos[6].setRotation(0);*/
 			
 			
 			if ("OP0004REDI".equals(idtramite))
 			{
-				textos[0].setX(13);
+				/*textos[0].setX(13);
 				textos[0].setY(530);
 				textos[4].setX(17);
 				textos[4].setY(520);
 				textos[5].setX(13);
 				textos[5].setY(510);
 				textos[6].setX(13);
-				textos[6].setY(500);
-				textos[1].setX(410);
-				textos[1].setY(763);
-				textos[2].setX(225);
+				textos[6].setY(500);*/
+				textos[0].setX(410);
+				textos[0].setY(763);
+				textos[1].setX(225);
+				textos[1].setY(25);
+				textos[2].setX(330);
 				textos[2].setY(25);
-				textos[3].setX(330);
-				textos[3].setY(25);
 				
 			}
 			else
 			{
-				textos[0].setX(13);
+				/*textos[0].setX(13);
 				textos[0].setY(545);
 				textos[4].setX(17);
 				textos[4].setY(535);
 				textos[5].setX(13);
 				textos[5].setY(525);
 				textos[6].setX(13);
-				textos[6].setY(515);
-				textos[1].setX(470);
-				textos[1].setY(745);
-				textos[2].setX(225);
+				textos[6].setY(515);*/
+				textos[0].setX(470);
+				textos[0].setY(745);
+				textos[1].setX(225);
+				textos[1].setY(27);
+				textos[2].setX(330);
 				textos[2].setY(27);
-				textos[3].setX(330);
-				textos[3].setY(27);
 			}
 			
+			textos[0].setRotation(0);
 			textos[1].setRotation(0);
 			textos[2].setRotation(0);
-			textos[3].setRotation(0);
-			((TextoStamp) textos[1]).setTexto((String) numeros.nextElement());
-			((TextoStamp) textos[2]).setTexto((String) referencias.nextElement());
-			((TextoStamp) textos[3]).setTexto((String) identificadores.nextElement());
+			((TextoStamp) textos[0]).setTexto((String) numeros.nextElement());
+			((TextoStamp) textos[1]).setTexto((String) referencias.nextElement());
+			((TextoStamp) textos[2]).setTexto((String) identificadores.nextElement());
 			
 			// Ejemplar administracion
-			((TextoStamp) textos[0]).setFontColor(Color.RED);
+			/*((TextoStamp) textos[0]).setFontColor(Color.RED);
 			((TextoStamp) textos[0]).setFontSize(TamFuente);
 			((TextoStamp) textos[0]).setTexto("Exemplar per a l'IBAVI");
 			((TextoStamp) textos[4]).setFontColor(Color.RED);
 			((TextoStamp) textos[4]).setFontSize(TamFuente);
 			((TextoStamp) textos[4]).setFontName("Helvetica-Oblique");
-			((TextoStamp) textos[4]).setTexto("Ejemplar para IBAVI");
+			((TextoStamp) textos[4]).setTexto("Ejemplar para IBAVI");*/
 			bis = new ByteArrayInputStream(documentoF.getDatosFichero());
 			bos = new ByteArrayOutputStream(sizeBuffer);
 			UtilPDF.stamp(bos,bis,textos);
 			administracion = bos.toByteArray();
 			
 			// Ejemplar empresa
-			textos[4].setX(13);
+			/*textos[4].setX(13);
 			((TextoStamp) textos[0]).setFontSize(TamFuente2);
 			((TextoStamp) textos[0]).setTexto("Exemplar per a l'ARRENDADOR");
 			((TextoStamp) textos[4]).setFontSize(TamFuente2);
@@ -180,10 +181,10 @@ public class FormateadorPdfIbavi extends FormateadorPdfFormularios{
 			bis = new ByteArrayInputStream(documentoF.getDatosFichero());
 			bos = new ByteArrayOutputStream(sizeBuffer);
 			UtilPDF.stamp(bos,bis,textos);
-			empresa = bos.toByteArray();
+			empresa = bos.toByteArray();*/
 			
 			// Ejemplar interesado
-			((TextoStamp) textos[0]).setFontSize(TamFuente3);
+			/*((TextoStamp) textos[0]).setFontSize(TamFuente3);
 			((TextoStamp) textos[0]).setTexto("Exemplar per a l'ENTITAT");
 			((TextoStamp) textos[4]).setFontName("Helvetica");
 			((TextoStamp) textos[4]).setFontSize(TamFuente3);
@@ -199,24 +200,31 @@ public class FormateadorPdfIbavi extends FormateadorPdfFormularios{
 			bis = new ByteArrayInputStream(documentoF.getDatosFichero());
 			bos = new ByteArrayOutputStream(sizeBuffer);
 			UtilPDF.stamp(bos,bis,textos);
-			interesado = bos.toByteArray();
+			interesado = bos.toByteArray();*/
 			
 			// Creamos input streams
 			iss[numIS] = new ByteArrayInputStream(administracion);
 			numIS++;
-			iss[numIS] = new ByteArrayInputStream(empresa);
+			/*iss[numIS] = new ByteArrayInputStream(empresa);
 			numIS++;
 			iss[numIS] = new ByteArrayInputStream(interesado);
-			numIS++;
-					
+			numIS++;*/
+			
 		}
 		
 		// Concatenamos todos los pdfs en uno
-		ByteArrayOutputStream pdfFinal = new ByteArrayOutputStream( 8192 + (plantilla.getArchivo().getDatos().length * numCopias * 3));
-		UtilPDF.concatenarPdf(pdfFinal,iss);
+		//ByteArrayOutputStream pdfFinal = new ByteArrayOutputStream( 8192 + (plantilla.getArchivo().getDatos().length * numCopias * 3));
+		if (numCopias > 1)
+		{
+			ByteArrayOutputStream pdfFinal = new ByteArrayOutputStream( 8192 + (plantilla.getArchivo().getDatos().length * numCopias));
+			UtilPDF.concatenarPdf(pdfFinal,iss);
+			// Devolvemos documento
+			documentoF.setDatosFichero(pdfFinal.toByteArray());
+		}
+		else{
+			documentoF.setDatosFichero(administracion);
+		}
 		
-		// Devolvemos documento
-		documentoF.setDatosFichero(pdfFinal.toByteArray());
 		return documentoF;
 	}
 
