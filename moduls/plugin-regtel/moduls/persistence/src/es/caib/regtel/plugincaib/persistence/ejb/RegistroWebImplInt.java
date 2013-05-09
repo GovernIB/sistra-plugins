3dfab79ff4f6b7227fdbf968f4193b9f16a9cdc5
rsanz-indra <rsanz-indra@5878cf19-a0df-4652-bd9f-a806d42fd1e1>
2013-05-09 14:27:04 +0000

    mid: 0000275 msg: Plugin registro CAIB: permitir usar interfaz ejb y webservice
    git-svn-id: https://svn.code.sf.net/p/sistra/code/plugins-caib/trunk@737 5878cf19-a0df-4652-bd9f-a806d42fd1e1

---
+++ moduls/plugin-regtel/moduls/persistence/src/es/caib/regtel/plugincaib/persistence/ejb/RegistroWebImplInt.java
@@ -0,0 +1,9 @@
+package es.caib.regtel.plugincaib.persistence.ejb;
+
+import es.caib.sistra.plugins.regtel.PluginRegistroIntf;
+
+public interface RegistroWebImplInt extends PluginRegistroIntf{
+
+	public void setPrincipal(String principal);
+	
+}
