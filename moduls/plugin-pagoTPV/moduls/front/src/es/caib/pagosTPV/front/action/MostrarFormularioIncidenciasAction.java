e6895942ae8177278f4aa650d21709712f5f3b9c
amaciam <amaciam@AMACIAM01W7.indra.es>
2017-12-05 17:41:26 +0100

    FIX #25 Corregir HELPDESK: Recuperar el NIF pels pagaments a l'auditoria de tramitació.

--- moduls/plugin-pagoTPV/moduls/front/src/es/caib/pagosTPV/front/action/MostrarFormularioIncidenciasAction.java
+++ moduls/plugin-pagoTPV/moduls/front/src/es/caib/pagosTPV/front/action/MostrarFormularioIncidenciasAction.java
@@ -2,6 +2,7 @@ package es.caib.pagosTPV.front.action;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,8 @@ import org.apache.struts.action.ActionMapping;
 import es.caib.pagosTPV.persistence.delegate.DelegateUtil;
 import es.caib.pagosTPV.persistence.delegate.SesionPagoDelegate;
 import es.caib.sistra.plugins.pagos.DatosPago;
+import es.caib.sistra.plugins.PluginFactory;
+import es.caib.sistra.plugins.login.PluginLoginIntf;
 import es.caib.util.StringUtil;
 
 /**
@@ -36,9 +39,17 @@ public class MostrarFormularioIncidenciasAction extends BaseAction
 		String idPersistencia = "";
 		String fechaCreacion = "";
 		String lang = "es";
+		String nivelAutenticacion = "";
 		
 		final SesionPagoDelegate dlg = getSesionPago(request);
 		DatosPago datosPago = dlg.obtenerDatosPago();
+		
+		Principal p = request.getUserPrincipal();
+		
+		PluginLoginIntf plgLogin = PluginFactory.getInstance().getPluginLogin();
+		
+		char metodoAuth = plgLogin.getMetodoAutenticacion(p);
+		String nivelAuth = Character.toString(metodoAuth);
 		
 		lang = datosPago.getIdioma();
 		idPersistencia = datosPago.getIdentificadorTramite();
@@ -47,7 +58,7 @@ public class MostrarFormularioIncidenciasAction extends BaseAction
 		procedimientoId = datosPago.getIdProcedimiento();
 		nif = datosPago.getNifDeclarante();
 		nombre = datosPago.getNombreDeclarante();
-		fechaCreacion = StringUtil.fechaACadena(datosPago.getFechaInicioTramite(), StringUtil.FORMATO_TIMESTAMP);			
+		fechaCreacion = StringUtil.fechaACadena(datosPago.getFechaInicioTramite(), StringUtil.FORMATO_TIMESTAMP);
 		
 		
 		 String contextoRaiz = DelegateUtil.getConfiguracionDelegate().obtenerPropiedad("sistra.contextoRaiz.front");
@@ -64,6 +75,7 @@ public class MostrarFormularioIncidenciasAction extends BaseAction
 			 url += "&nombre=" + encodeParameter(nombre);
 		 }
 		 url += "&fechaCreacion=" + encodeParameter(fechaCreacion);
+		 url += "&nivelAutenticacion=" + encodeParameter(nivelAuth);
 		 
 		 response.sendRedirect(url);		 
 		 return null;
